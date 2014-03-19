7619fd1eb4880e6b59c90fd3f63b8cccd1bf5dc3
kotatuinu <kotatuinu@nifty.com>
2014-03-19 23:38:45 +0900

    作成

---
+++ README.md
@@ -0,0 +1,20 @@
+# ffmepgを使用して動画ファイルからMP3ファイルを生成するPowerShellスクリプト
+ 主な目的は、ニコニコ動画からダウンロードした動画ファイル（ファイル名：<動画番号>.mp4）を、動画番号から動画情報を取得して、MP3ファイルに変換＋MP3タグIDを設定する。
+
+##使い方
+変換対象の動画ファイル、mp3ファイルを格納しているディレクトリを指定して、出力先ディレクトリに出力します。
+拡張子がmp3,mp4,swf,flvのファイルをffmpegで変換します。
+(拡張子を含まない)ファイル名が、nm,smで始まるとき、ニコニコ動画APIの動画情報取得を使用して、タイトルや製作者名を取得。MP3タグIDに設定します。album名は”VOCALOID”固定。
+
+ffmpegのディレクトリは、スクリプトの以下に埋め込んであるので、適宜、書き換えてください。
+
+set-variable -name FFMPEG_PATH -value "D:\tools\ffmpeg-20140228-git-669043d-win64-shared\bin" -option constant
+
+
+PS > get-nicovideo.ps1 -u <UserID> -p <Password> -o <Output Directory> -movie_no <Movie No>[,<Movie No>...]
+-- -i : 変換対象の動画ファイル、mp3ファイルを格納しているディレクトリ
+-- -o : 出力先ディレクトリ
+-- -movie_no : ダウンロードしたい動画番号。カンマで区切ることで複数指定可能
+
+
+
---
+++ conv-nicovideo.ps1
@@ -0,0 +1,135 @@
+set-variable -name FFMPEG_PATH -value "D:\tools\ffmpeg-20140228-git-669043d-win64-shared\bin" -option constant
+set-variable -name FFMPEG -value "ffmpeg.exe" -option constant
+set-variable -name NICOVIDEO_API -value "http://ext.nicovideo.jp/api/getthumbinfo/" -option constant
+
+
+# Usage
+$Usages = @(
+	"Niconicovideo's Movie Number Named files(mp4,flv,swf) convert MP3, and MP3 File(Movie Number Named) set MP3 TAG ID.";
+	"Output MP3 File Name <Title>_<MovieNumber>.mp3.";
+	" Usage : $MyInvocation.MyCommand.Name  -i <InputPath> -o <OutputPath>";
+	"  OPTIONS";
+	"  -i : InputFiles Directory.";
+	"  -o : Output Directory.";
+)
+
+function Output-Usage {
+	$Usages
+}
+
+
+$ArgMap = @{ "-i" = ""; "-o" = ""}
+
+#Argument Get & Check
+function Get-Argument($ArgList) {
+	if($ArgList.Count -lt $ArgMap.Count*2) {
+		return $false
+	}
+
+	$ArgKey=""
+	$ArgList | ForEach-Object {
+		if($ArgKey -eq "") {
+			if($ArgMap.ContainsKey($_)) {
+				$ArgKey=$_
+			} else {
+				return $false
+			}
+		} else {
+			$ArgMap[$ArgKey]=$_
+			$ArgKey=""
+		}
+	}
+	return $true
+}
+
+
+#“®‰æ”Ô†‚©‚ç“®‰æî•ñiXMLj‚ðŽæ“¾
+function Get-NicoInfo($movie_no) {
+	$uri="$NICOVIDEO_API$movie_no"
+	$encode_utf8 = [Text.Encoding]::GetEncoding("utf-8")
+	$wc = New-Object System.Net.WebClient
+	$wc.Encoding=$encode_utf8
+
+	#
+	return [xml]$wc.DownloadString($uri)
+}
+
+
+if(-not(Get-Argument($Args))){
+	[Console]::WriteLine("ERROR: Invalid Arguments.")
+	Output-Usage
+	exit
+}
+
+if(-not(Test-Path -LiteralPath $ArgMap["-i"] -PathType Container)) {
+	[Console]::WriteLine("ERROR: Not Exist InputDirectory.")
+	Out-Usage
+	exit
+}
+if(-not(Test-Path -LiteralPath $ArgMap["-o"] -PathType Container)) {
+	[Console]::WriteLine("ERROR: Not Exist OutputDirectory.")
+	Out-Usage
+	exit
+}
+
+if($ArgMap["-i"].LastIndexOf("\")+1 -eq $ArgMap["-i"].Length) {
+	$ArgMap["-i"].Remove($ArgMap["-i"].LastIndexOf("\"), 1)
+}
+if($ArgMap["-o"].LastIndexOf("\")+1 -eq $ArgMap["-o"].Length) {
+	$ArgMap["-o"].Remove($ArgMap["-o"].LastIndexOf("\"), 1)
+}
+
+$inPath = $ArgMap["-i"]
+$outPath = $ArgMap["-o"]
+
+Get-ChildItem $ArgMap["-i"] | Where-Object { $_.Name -match '((.+)\.(mp3|mp4|swf|flv))'} | ForEach-Object {
+
+	$file_name = $Matches[0];
+	$name = $Matches[2];
+	$ext = $Matches[3];
+
+	if($file_name -match '(((sm|nm)\d+)(|_low)\.(mp3|mp4|swf|flv))') {
+
+		# Get Movie Info by movie no.
+		$movie_no = $Matches[2];
+
+		$xmldata = Get-NicoInfo($movie_no)
+
+		if($xmldata.nicovideo_thumb_response.status -eq "ok") {
+
+			$title = $xmldata.nicovideo_thumb_response.thumb.title -replace "[/?:*`"><|\\]", ""
+			$user_nickname = $xmldata.nicovideo_thumb_response.thumb.user_nickname
+			$year = Get-Date $xmldata.nicovideo_thumb_response.thumb.first_retrieve -Format "yyyy"
+
+			$id_tag = "-metadata title=`"${title}`" -metadata year=`"${year}`" -metadata creator=`"${user_nickname}`" -metadata album=`"VOCALOID`" -id3v2_version 3"
+			$output_file = "${movie_no}_${title}.mp3"
+
+		} else {
+
+			Write-Output "Movie[${movie_no}] deleted. FileName = ${filename}"
+			$id_tag = "-metadata title=`"${name}`" -metadata album=`"VOCALOID`" -id3v2_version 3"
+			$output_file = "${movie_no}.mp3"
+		}
+
+	} else {
+		$id_tag = "-metadata title=`"${name}`" -metadata album=`"VOCALOID`" -id3v2_version 3"
+		if($file_name -match '(.+?)\.(mp3|mp4|swf|flv)') {
+			$output_file = $Matches[1] + ".mp3"
+		} else {
+			continue
+		}
+	}
+
+	if($ext -ne "mp3") {
+		# When not mp3 file, convert mp3.
+		$mp3_cnv = "-ar 48000 -ab 128k -f mp3"
+	} else {
+		$mp3_cnv = ""
+	}
+
+	$command = "${FFMPEG_PATH}\${FFMPEG} -y -i ${inPath}\${file_name} ${id_tag} ${mp3_cnv} `"${outPath}\${output_file}`""
+
+	Write-Output $command
+	Invoke-Expression $command
+
+}
